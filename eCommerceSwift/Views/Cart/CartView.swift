2bcbee2c4ab8266597caae242fbca58005d9f0c9
Ben Koo <61488202+sebkoo@users.noreply.github.com>
2025-06-08 21:07:35 -0400

    Init: setup project structure and basic TabView

---
+++ eCommerceSwift/Views/Cart/CartView.swift
@@ -0,0 +1,18 @@
+//
+//  CartView.swift
+//  eCommerceSwift
+//
+//  Created by Ben Koo on 6/8/25.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    var body: some View {
+        Text("Cart")
+    }
+}
+
+#Preview {
+    CartView()
+}
