30e9b022b1ed183870f985adb0a0e7e280f07545
Ben Koo <61488202+sebkoo@users.noreply.github.com>
2025-06-08 21:01:36 -0400

    Initial Commit

---
+++ eCommerceSwift/eCommerceSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  eCommerceSwiftApp.swift
+//  eCommerceSwift
+//
+//  Created by Ben Koo on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct eCommerceSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
